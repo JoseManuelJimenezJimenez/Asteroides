523db0b4f37803c7bcba2d7aaef23b7432e2a0af
JoseManuelJimenezJimenez <jimenezjimenezjosemanuel@gmail.com>
2018-04-30 00:13:08 +0200

    Practica asteroides parte 1 y 2

---
+++ Asteroides/app/src/main/java/com/example/jose/asteroides/Asteroides.java
@@ -0,0 +1,13 @@
+package com.example.jose.asteroides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Asteroides extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
